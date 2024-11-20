21b5e2ce3bf52233183d7bffae8771eb7291308c
tewarishash12 <tewarishashwat2002@gmail.com>
2024-11-20 15:27:57 +0530

    Added outer paths

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import ExpenseRecord from './pages/ExpenseRecord';
+import ExpenseRecords from './pages/ExpenseRecords';
 import Form from './pages/Form';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
       <>
         <Router>
+          <Navbar/>
           <Routes>
             <Route path='/' element={<HomePage/>}/>
             <Route path='/addexpense' element={<Form/>}/>
-            <Route path='/history' element={<ExpenseRecord />}/>
+            <Route path='/history' element={<ExpenseRecords />}/>
           </Routes>
         </Router>
       </>
